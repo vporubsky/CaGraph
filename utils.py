b5afe05cbe6f245479677c82a5c3dcf4f730fe79
Veronica Porubsky <verosky@uw.edu>
2023-01-20 12:10:19 -0800

    Updated threshold analysis to include context selectivity information.

---
+++ utils.py
@@ -0,0 +1,121 @@
+"""
+Developer Name: Veronica Porubsky
+Developer ORCID: 0000-0001-7216-3368
+Developer GitHub Username: vporubsky
+Developer Email: verosky@uw.edu
+
+File Creation Date: 
+File Final Edit Date:
+
+Description: A utilities package to assist with analyses
+
+Todo: eventually this will need to be incorporated into a formal submodule
+"""
+# Import packages
+import numpy as np
+
+
+def bins(lst, n):
+    """Yield successive n-sized chunks from lst."""
+    lst = list(lst)
+    build_binned_list = []
+    for i in range(0, len(lst), n):
+        build_binned_list.append(lst[i:i + n])
+    return build_binned_list
+
+# Function definitions
+def generate_randomized_timeseries_matrix(data: list) -> np.ndarray:
+    """
+    data: list
+
+    Parameter data should contain a list of np.ndarray objects.
+
+    Return a numpy array or NWB file.
+    """
+    time = data[0, :].copy()
+    for row in range(np.shape(data)[0]):
+        np.random.shuffle(data[row, :])
+    data[0, :] = time.copy()
+    return data
+
+def generate_randomized_timeseries_binned(data: list, bin_size: int) -> np.ndarray:
+    """
+    data: list
+
+    Parameter data should contain a list of np.ndarray objects.
+
+    Return a numpy array or NWB file.
+    """
+    time = data[0, :].copy()
+    build_new_array = np.array(bins(lst=data[1, :], n=bin_size))
+
+    # build binned dist
+    for row in range(np.shape(data[2:, :])[0]):
+        binned_row = bins(lst=data[row + 2, :], n=bin_size)
+        build_new_array = np.vstack([build_new_array, binned_row])
+
+    for row in range(np.shape(build_new_array)[0]):
+        np.random.shuffle(build_new_array[row, :])
+
+    flatten_array = time.copy()
+    for row in range(np.shape(build_new_array)[0]):
+        flat_row = [item for sublist in build_new_array[row, :] for item in sublist]
+        flatten_array = np.vstack([flatten_array, flat_row])
+
+    return flatten_array
+
+def event_bins(data, events):
+    """
+    :param data:
+    :param events: single events timecourse
+    :return:
+    """
+    data = list(data)
+    build_binned_list = []
+    event_idx = list(np.nonzero(events)[0])
+    if event_idx[-1] != len(data):
+        event_idx.append(len(data))
+    start_val = 0
+    for idx in event_idx:
+        build_binned_list.append(data[start_val:idx])
+        start_val = idx
+    np.random.shuffle(build_binned_list)
+    flat_random_binned_list = [item for sublist in build_binned_list for item in sublist]
+    return flat_random_binned_list
+
+
+def generate_event_segmented(data: list, event_data: list) -> np.ndarray:
+    """
+    data: list
+
+    Parameter data should contain a list of np.ndarray objects.
+
+    Return a numpy array or NWB file.
+    """
+    time = data[0, :].copy()
+
+    # build binned dist
+    flatten_array = time.copy()
+    for row in range(np.shape(data[1:, :])[0]):
+        binned_row = event_bins(data=data[row + 1, :], events = event_data[row + 1, :])
+        flatten_array = np.vstack([flatten_array, binned_row])
+
+    return flatten_array
+
+def generate_randomized(data: list, bin_size: int) -> np.ndarray:
+    """
+    data: list
+
+    Parameter data should contain a list of np.ndarray objects.
+
+    Return a numpy array or NWB file.
+    """
+    time = data[0, :].copy()
+
+    # build binned dist
+    flatten_array = time.copy()
+    for row in range(np.shape(data[2:, :])[0]):
+        binned_row = bins(lst=data[row + 2, :], n=bin_size)
+        flatten_array = np.vstack([flatten_array, binned_row])
+
+    return flatten_array
